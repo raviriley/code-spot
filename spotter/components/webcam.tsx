0e4f765e86145b39b1b0d00cb08ecb8f857a0342
Ravi Riley <raviriley@gmail.com>
2023-10-29 07:26:39 -0700

    Fix webcam component

--- spotter/components/webcam.tsx
+++ spotter/components/webcam.tsx
@@ -9,29 +9,28 @@ export default function WebCam({
 }: {
   className?: string;
 }) {
-  const [isMounted, setIsMounted] = useState(false);
-  const [useDefaultCamera, setUseDefaultCamera] = useState(false);
+  const [videoLoaded, setVideoLoaded] = useState(false);
+  const [imageError, setImageError] = useState(false);
   const videoRef = useRef<HTMLVideoElement | null>(null);
 
   useEffect(() => {
-    setIsMounted(true);
-
-    if (useDefaultCamera) {
-      navigator.mediaDevices
-        .getUserMedia({ video: true })
-        .then((stream) => {
-          let video = videoRef.current;
-          if (!video) throw new Error("Video ref not found");
-          video.srcObject = stream;
+    navigator.mediaDevices
+      .getUserMedia({ video: true })
+      .then((stream) => {
+        let video = videoRef.current;
+        if (!video) throw new Error("Video ref not found");
+        video.srcObject = stream;
+        video.onloadedmetadata = () => {
           video.play();
-        })
-        .catch((err) => {
-          console.log("Something went wrong: ", err);
-        });
-    }
+          setVideoLoaded(true);
+        };
+      })
+      .catch((err) => {
+        console.log("Something went wrong: ", err);
+        setImageError(true);
+      });
 
     return () => {
-      setIsMounted(false);
       if (
         videoRef.current &&
         videoRef.current.srcObject &&
@@ -40,23 +39,22 @@ export default function WebCam({
         videoRef.current.srcObject.getTracks().forEach((track) => track.stop());
       }
     };
-  }, [useDefaultCamera]);
+  }, []);
 
   const handleImageError = () => {
-    setUseDefaultCamera(true);
+    setImageError(true);
   };
 
   return (
     <div className={cn("")}>
-      {isMounted && !useDefaultCamera && (
+      {videoLoaded && !imageError ? (
         <img
           src="http://localhost:5001/video"
           className="transform scaleX[-1] rounded-lg"
           alt="Video Feed"
           onError={handleImageError}
         />
-      )}
-      {isMounted && useDefaultCamera && (
+      ) : (
         <video
           ref={videoRef}
           autoPlay
@@ -64,12 +62,10 @@ export default function WebCam({
           className="transform scaleX[-1] rounded-lg"
         ></video>
       )}
-      {!isMounted && (
-        <>
-          <Skeleton className="h-32 w-full rounded-lg">
-            <CameraIcon className="w-12 h-12 text-gray-500 mx-auto" />
-          </Skeleton>
-        </>
+      {!videoLoaded && (
+        <Skeleton className="h-32 w-full rounded-lg">
+          <CameraIcon className="w-12 h-12 text-gray-500 mx-auto" />
+        </Skeleton>
       )}
     </div>
   );
